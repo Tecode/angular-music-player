77eea9674eecb7510c85ee0e94683c1d25c00ad2
Tecode <283731869@qq.com>
2019-04-05 17:51:20 +0800

    feat: --

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,33 +9,20 @@ import { NavBarIcon } from 'src/interface';
 export class NavbarComponent implements OnInit {
   iconArr: NavBarIcon[] = [
     {
-      normalUrl: 'assets/imgs/navbar/icon_flash@2x.svg',
-      text: '热门',
+      text: '推荐',
       routerLink: '/hot',
-      activeUrl: 'assets/imgs/navbar/icon_flash_active@2x.svg',
       id: 10001
     },
     {
-      normalUrl: 'assets/imgs/navbar/icon_music@2x.svg',
-      text: '列表',
+      text: '排行',
       routerLink: '/list',
-      activeUrl: 'assets/imgs/navbar/icon_music_active@2x.svg',
       id: 10001
     },
     {
-      normalUrl: 'assets/imgs/navbar/icon_search@2x.svg',
       text: '搜索',
       routerLink: '/search',
-      activeUrl: 'assets/imgs/navbar/icon_search_active@2x.svg',
       id: 10001
-    },
-    {
-      normalUrl: 'assets/imgs/navbar/icon_profile@2x.svg',
-      text: '我的',
-      routerLink: '/profile',
-      activeUrl: 'assets/imgs/navbar/icon_profile_active@2x.svg',
-      id: 10001
-    },
+    }
   ];
   constructor() { }
 
--- src/interface.ts
+++ src/interface.ts
@@ -1,7 +1,5 @@
 export interface NavBarIcon {
-    normalUrl: string;
     activeUrl: string;
     text: string;
-    routerLink: string;
     id: number;
 }
