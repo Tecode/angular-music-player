8555452f92a05164962b9471676c964ee2f253fb
Tecode <283731869@qq.com>
2019-04-16 23:37:50 +0800

    '解决服务端动画问题'

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -14,29 +14,53 @@ const animatePath: string[] = [
 
 // 路由切换过渡动画
 export const slideInAnimation =
-    trigger('routeAnimations', animatePath.map(path => (
-        transition(path, [
-            style({ position: 'relative' }),
-            query(':enter, :leave', [
-                style({
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100%'
-                })
-            ]),
-            query(':enter', [
-                style({ left: '-100%' })
-            ]),
-            query(':leave', animateChild()),
-            group([
-                query(':leave', [
-                    animate('300ms ease-out', style({ left: '100%' }))
-                ]),
-                query(':enter', [
-                    animate('300ms ease-out', style({ left: '0%' }))
-                ])
-            ]),
-            query(':enter', animateChild()),
+trigger('routeAnimations', [
+    transition('hot <=> search', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%'
+        })
+      ]),
+      query(':enter', [
+        style({ left: '-100%'})
+      ]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('300ms ease-out', style({ left: '100%'}))
+        ]),
+        query(':enter', [
+          animate('300ms ease-out', style({ left: '0%'}))
         ])
-    )));
+      ]),
+      query(':enter', animateChild()),
+    ]),
+    transition('hot <=> list', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%'
+        })
+      ]),
+      query(':enter', [
+        style({ left: '-100%'})
+      ]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('200ms ease-out', style({ left: '100%'}))
+        ]),
+        query(':enter', [
+          animate('300ms ease-out', style({ left: '0%'}))
+        ])
+      ]),
+      query(':enter', animateChild()),
+    ])
+  ]);
