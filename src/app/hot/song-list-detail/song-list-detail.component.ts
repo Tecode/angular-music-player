b8d96b053bac3d5706f145a1bfcd362cc561d0a2
Tecode <283731869@qq.com>
2019-04-22 00:36:43 +0800

    优化详情页，增加下拉模糊放大效果

--- src/app/hot/song-list-detail/song-list-detail.component.ts
+++ src/app/hot/song-list-detail/song-list-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { Observable } from 'rxjs';
 import {
   trigger,
@@ -35,19 +35,29 @@ import { Position } from '../../common/scroll/scroll.component';
   ]
 })
 export class SongListDetailComponent implements OnInit {
+  @ViewChild('coverImage') coverImage: ElementRef;
+  @ViewChild('scrollEl') scrollEl: ElementRef;
+  @ViewChild('filterEl') filterEl: ElementRef;
+  @ViewChild('playButtonEl') playButtonEl: ElementRef;
+  @ViewChild('layerFillEl') layerFillEl: ElementRef;
+
   public detailStore$: Observable<HotState>;
-  public scrollTop: number = 260;
   public songDetailList: SongListDetail = {
     coverImgUrl: '',
     name: '',
     listData: []
   };
-  public isShow:boolean = true;
+  public isShow: boolean = true;
+
+  private static fixedHeight: number = 40;
+  private scrollTop: number = 260;
+  private coverImageHeight: number;
 
   constructor(
     public router: Router,
     private store: Store<{ hotStore: HotState }>,
-    private activeRouter: ActivatedRoute
+    private activeRouter: ActivatedRoute,
+    private renderer: Renderer2
   ) {
     this.detailStore$ = store.pipe(select('hotStore'));
   }
@@ -58,6 +68,15 @@ export class SongListDetailComponent implements OnInit {
     this.detailStore$.subscribe(data => {
       this.songDetailList = data.songListDetail;
     })
+  }
+
+  ngAfterViewInit(): void {
+    // 获取背景图高度
+    this.coverImageHeight = this.coverImage.nativeElement.clientHeight;
+    console.log(this.coverImage.nativeElement.style, '-----------');
+    // 设置top高度
+    // 使用renderer：Renderer修改样式
+    this.renderer.setStyle(this.scrollEl.nativeElement, 'top', `${this.coverImageHeight}px`);
   }
 
 
@@ -72,15 +91,45 @@ export class SongListDetailComponent implements OnInit {
   }
 
   public handlerScroll(position: Position) {
-    if (this.scrollTop > 260) {
-      this.scrollTop = 260;
-      return;
+    // 当触发滚动时      
+    let minScrollY = -this.coverImageHeight + SongListDetailComponent.fixedHeight;
+    let moveY = Math.max(minScrollY, position.y);
+    let zIndex = 0;
+
+    // 当向上推得时候填充背景
+    this.renderer.setStyle(this.layerFillEl.nativeElement, 'transform', `translate3d(0 ,${moveY}px, 0)`);
+    this.renderer.setStyle(this.layerFillEl.nativeElement, 'webkit-transform', `translate3d(0 ,${moveY}px, 0)`);
+
+
+    // 下拉放大、上拉模糊
+    let scale = 1;
+    let blur = 0;
+    const formula = Math.abs(position.y / this.coverImageHeight);
+
+    if (position.y > 0) {
+      zIndex = 10;
+      scale = 1 + formula;
+      this.renderer.setStyle(this.coverImage.nativeElement, 'transform', `scale(${scale})`);
+      this.renderer.setStyle(this.coverImage.nativeElement, 'webkitTransform', `scale(${scale})`);
+    } else {
+      blur = Math.min(20 * formula, 20);
+      this.renderer.setStyle(this.filterEl.nativeElement, 'backdrop-filter', `blur(${blur}px)`);
+      this.renderer.setStyle(this.filterEl.nativeElement, 'webkitBackdrop-filter', `blur(${blur}px)`);
     }
-    if (this.scrollTop < 38) {
-      this.scrollTop = 38;
-      return;
+
+    // 不推到顶，留一部分
+    if (position.y < minScrollY) {
+      zIndex = 10;
+      this.renderer.setStyle(this.coverImage.nativeElement, 'padding-top', 0);
+      this.renderer.setStyle(this.coverImage.nativeElement, 'height', `${SongListDetailComponent.fixedHeight}px`);
+      // 隐藏 随机播放全部 按钮
+      this.renderer.setStyle(this.playButtonEl.nativeElement, 'display', 'none');
+    } else {
+      this.renderer.setStyle(this.coverImage.nativeElement, 'padding-top', '70%');
+      this.renderer.setStyle(this.coverImage.nativeElement, 'height', '0');
+      // 显示 随机播放全部 按钮
+      this.renderer.setStyle(this.playButtonEl.nativeElement, 'display', 'block');
     }
-    this.scrollTop = this.scrollTop + position.y;
-    console.log(position);
+    this.renderer.setStyle(this.coverImage.nativeElement, 'z-index', zIndex);
   }
 }
